660cdd72ec246e970a23ad3cb82354d4e8c454c6
Matthew Wang <wmatthew123@gmail.com>
2019-01-26 22:50:37 +0800

    added buy pegga button

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pegga Pig Toy Store`,
+    description: `Get your latest Pegga Pig Toys Here! Year of the pig. Chinese new year sale!`,
+    author: `@Mato_wang`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,17 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi Peggas</h1>
+    <button
+      class="snipcart-add-item"
+      data-item-id="2"
+      data-item-name="Pegga Pig Toy"
+      data-item-price="300.00"
+      data-item-weight="20"
+      data-item-url="https://angry-meitner-03adf1.netlify.com/"
+      data-item-description="Fun toy for the Year of the Pig">
+      Buy Pegga Toy
+    </button>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
