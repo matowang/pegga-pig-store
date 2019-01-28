27cf616b7a6bcd94b5c6b4524d1e537e0321c51e
Matthew Wang <wmatthew123@gmail.com>
2019-01-29 01:05:41 +0800

    rendered products

--- src/components/product-card.js
+++ src/components/product-card.js
@@ -1,10 +1,47 @@
 import React from 'react';
-const ProductCard = ({ title, price, imgSrc }) => {
+import { StaticQuery, graphql } from 'gatsby';
+
+const ProductCard = ({ id, title, price, imgUrl, width, length, height }) => {
     return (
         <div className="product-card">
-            {/* <img src={imgSrc} /> */}
+
+            <StaticQuery query={graphql`
+                query {
+                    allFile(filter: { relativePath: { regex: "/products//" } }) {
+                        edges {
+                          node {
+                            publicURL
+                            relativePath
+                          } 
+                        }
+                      }
+                }
+            `}
+                render={data =>
+                    <img
+                        src={data.allFile.edges.filter(
+                            ({ node }) => {
+                                return node.relativePath === imgUrl
+                            })[0].node.publicURL
+                        }
+                        alt={title}
+                    />}
+            />
             <h2>{title}</h2>
             <div className="product-card-price">{price}</div>
+            <button
+                className="snipcart-add-item"
+                data-item-id={id}
+                data-item-name={title}
+                data-item-price={price}
+                data-item-url="https://angry-meitner-03adf1.netlify.com/"
+                data-item-image={`https://angry-meitner-03adf1.netlify.com${imgUrl}`}
+                data-item-width={width}
+                data-item-length={length}
+                data-item-height={height}
+            >
+                Buy
+            </button>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,31 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import ProductCard from '../components/product-card'
 
 class IndexPage extends React.Component {
   render() {
-    console.log(this.props.data);
     return (
       <Layout>
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
         <h1>Hi Peggas</h1>
-        <button
-          className="snipcart-add-item"
-          data-item-id="2"
-          data-item-name="Pegga Pig Toy"
-          data-item-price='{"hkd": 200.0, "twd": 1200}'
-          data-item-weight="20"
-          data-item-url="https://angry-meitner-03adf1.netlify.com/"
-          data-item-description="Fun toy for the Year of the Pig">
-          Buy Pegga Toy
-        </button>
-        <Link to="/test-page">test page</Link>
+        {this.props.data.allProductsJson.edges.map(({ node }) => {
+          const { id, title, price, dimensions, imgs } = node;
+          return (
+            <ProductCard
+              id={id}
+              title={title}
+              price={price.hkd}
+              imgUrl={imgs[0]}
+              width={dimensions.w}
+              length={dimensions.l}
+              height={dimensions.h}
+              key={id}
+            />
+          )
+        })}
       </Layout>
     )
   }
@@ -34,10 +38,17 @@ query {
   allProductsJson {
     edges {
       node {
+        id
         title
         price {
           hkd
           twd
+        }
+        imgs
+        dimensions {
+          w
+          l
+          h
         }
       }
     }
